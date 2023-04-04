75c66f9968e4391c94a653b50a75560b5d9c58a6
ben-gee723 <ben.m.goldschmidt@gmail.com>
2023-04-04 10:57:19 +0200

    data updated

--- src/components/LinkButtons/LinkButton/index.jsx
+++ src/components/LinkButtons/LinkButton/index.jsx
@@ -6,13 +6,15 @@ export default function LinkButton ({linkInfo}) {
     //     link: "https://drive.google.com/drive/u/1/",
     //     img: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Google_Drive_icon_%282020%29.svg/1147px-Google_Drive_icon_%282020%29.svg.png"
     // }
-    console.log("linkInfo", linkInfo)
+    // console.log("linkInfo", linkInfo)
     const { name, img, link } = linkInfo;
     
     return(
         <div className="link-button">
             <a href={link}>
-                <img src={img ? img : qMark} alt={name}></img>
+                <div className='img-container'>
+                    <img src={img ? img : qMark} alt={name}></img>
+                </div>
                 <h3>{name}</h3>
             </a>
         </div>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,7 @@
 import LinkButtons from "../../components/LinkButtons"
 export default function Home (){
     return (
-        <div className="home">
+        <div className="home page">
             <h1>All App Links</h1>
             <div className="apps-container">
                 <LinkButtons/>
