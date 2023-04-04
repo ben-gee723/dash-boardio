5cf23ca2037da461df8ba174f67d6537e07ea56b
ben-gee723 <ben.m.goldschmidt@gmail.com>
2023-04-04 19:48:00 +0200

    MyTeam Bild verlinkt

--- src/pages/MyTeam/MyTeam.jsx
+++ src/pages/MyTeam/MyTeam.jsx
@@ -4,9 +4,11 @@ export default function MyTeams() {
     return(
         <div className="page my-team">
             <h1> My Team </h1>
-            <div className='image-container'>
-                <img src={MyTeamsImage} alt="Diagram of my team" />
-            </div>
+            <a target="_blank" href="https://my-dashboard-delta.vercel.app/assets/DCI%20CM%20-%20My%20Team%20-%20Ben%20G-c35639bb.png">
+                <div className='image-container'>
+                    <img src={MyTeamsImage} alt="Diagram of my team" />
+                </div>
+            </a>
         </div>
     )
 }
